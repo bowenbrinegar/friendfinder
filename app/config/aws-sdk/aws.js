4e040bb20068d8c3d32b5ed474c3e3f0376b0c47
bowen <bowenbrinegar@gmail.com>
2017-12-27 21:54:15 -0500

    update creds

--- app/config/aws-sdk/aws.js
+++ app/config/aws-sdk/aws.js
@@ -1,14 +1,14 @@
 var AWS = require('aws-sdk');
 
 AWS.config.update({accessKeyId: 'AKID', secretAccessKey: 'SECRET'});
-AWS.config.loadFromPath('./config/credentials.json');
+! async function run() { AWS.config.loadFromPath('/Users/bowenbrinegar/aws/config/credentials.json'); }()
 
 module.exports = function() {
 	var s3 = new AWS.S3();
 	return s3
 }
 
-s3.getBucketAcl({Bucket: 'friendfinder192837465'}, function(err, data) {
-	if (err) console.log(err, err.stack); // an error occurred
-	else     console.log(data);           // successful response
-});
+/*s3.getBucketAcl({Bucket: 'friendfinder192837465'}, function(err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else     console.log(data);           // successful response
+	});*/

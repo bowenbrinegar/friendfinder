9e5bc4679709cd4338d124ba5c29e11249847606
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-01-09 17:13:05 -0500

    IAM aws creds

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 const AWS = require('aws-sdk');
+const db = require("./app/models");
+
 const busboyBodyParser = require('busboy-body-parser');
+const bodyParser = require("body-parser");
 
 const passport = require('passport');
 const session = require('express-session');
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-const db = require("./app/models");
 
 app.use(busboyBodyParser({ limit: '5mb' }));
 app.use(bodyParser.json());
@@ -29,8 +29,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-AWS.config.update({accessKeyId: 'AKID', secretAccessKey: 'SECRET'});
-AWS.config.loadFromPath('/Users/bowenbrinegar/aws/config/credentials.json');
+AWS.config.loadFromPath('./app/config/credentials.json');
 const s3 = new AWS.S3();
 
 require('./app/config/passport/passport.js')(passport, db.Users);

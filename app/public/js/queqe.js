d5e5106bfaf5e26c4a4ad7f53c48e9badbc386d2
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-01-14 13:10:18 -0500

    First Steps

--- app/public/js/queqe.js
+++ app/public/js/queqe.js
@@ -1,29 +1,63 @@
 $(document).ready(function () {
   let count = 0;
   let count2 = 0;
+  let turn = true;
 
   //render functions
 
-  function renderQueqe (response) {
-    let html = `<img class='imgQueqe' src=${response.url} data-id=${response.id}>`
-    $('#queqe').prepend(html)
-  };
-
-  function renderOne (response) {
-    let html = `<img class='imgQueqe' style='display:block' src=${response.url} data-id=${response.id}>`
-    $('#queqe').append(html)
-  };
-
-  function renderOnView (res) {
-    let $img = $('.profileOnView img');
-    $img.removeAttr('src');
-    $img.attr('src', res.url);
-    $img.attr('data-id', res.id)
-    if (count > 0) {
-      fetchOne();
+  class Render {
+    static queqe(response) {
+      let html = `<img class='imgQueqe' src=${response.url} data-id=${response.id}>`
+      $('#queqe').prepend(html)
     }
-    count++
+    static one(response) {
+      let html = `<img class='imgQueqe' style='display:block' src=${response.url} data-id=${response.id}>`
+      $('#queqe').append(html)
+    }
+    static onView (res) {
+      let $img = $('.profileOnView img');
+      $img.removeAttr('src');
+      if (res === 'n/a') {
+        $img.attr('src', './assets/imgs/profile-female.jpg');
+        turn = false;
+        return
+      }
+      $img.attr('src', res.url);
+      $img.attr('data-id', res.id)
+      turn = true
+      if (count > 0) {
+        fetchOne();
+      }
+      count++
+    }
   }
+
+  // function renderQueqe (response) {
+  //   let html = `<img class='imgQueqe' src=${response.url} data-id=${response.id}>`
+  //   $('#queqe').prepend(html)
+  // };
+
+  // function renderOne (response) {
+  //   let html = `<img class='imgQueqe' style='display:block' src=${response.url} data-id=${response.id}>`
+  //   $('#queqe').append(html)
+  // };
+
+  // function renderOnView (res) {
+  //   let $img = $('.profileOnView img');
+  //   $img.removeAttr('src');
+  //   if (res === 'n/a') {
+  //     $img.attr('src', './assets/imgs/profile-female.jpg');
+  //     turn = false;
+  //     return
+  //   }
+  //   $img.attr('src', res.url);
+  //   $img.attr('data-id', res.id)
+  //   turn = true
+  //   if (count > 0) {
+  //     fetchOne();
+  //   }
+  //   count++
+  // }
 
   function alertFunc(res) {
     alert('match!');
@@ -70,11 +104,12 @@ $(document).ready(function () {
       type: 'POST',
       url: '/fetch-next',
       data: obj,
-      success: renderOnView
+      success: Render.onView
     })
   }
 
   function createMatch(res) {
+    if (res === 'n/a') { fetchNext(); return }
     $.ajax({
       type: 'POST',
       url: '/create-match',
@@ -94,31 +129,11 @@ $(document).ready(function () {
       type: 'POST',
       url: '/match-checker',
       data: obj,
-      codeStatus: {
-        404: fetchNext()
-      },
       success: createMatch
     })
   }
 
-  //on-click profile on view ajax
-
-  $('.selectContainer').on('click', '.imgQueqe', function(evt) {
-    evt.stopPropagation();
-    let id = {id: $(this).attr('data-id')}
-    $.ajax({
-      type: 'POST',
-      url: '/load-on-view',
-      data: id,
-      success: renderOnView
-    })
-    $(this).remove()
-  })
-
-  //on-click like ajax post
-
-  $('#like').on("click", function(evt) {
-    evt.stopPropagation();
+  function like() {
     let obj = {choiceId: $('.profileOnView img').attr('data-id')};
     $.ajax({
       type: 'POST',
@@ -131,11 +146,9 @@ $(document).ready(function () {
         }
       }
     });
-  });
+  }
 
-
-  $('#dislike').on("click", function(evt) {
-    evt.stopPropagation();
+  function dislike() {
     let obj = {choiceId: $('.profileOnView img').attr('data-id')};
     $.ajax({
       type: 'POST',
@@ -143,6 +156,40 @@ $(document).ready(function () {
       data: obj,
       success: fetchNext
     });
+  }
+
+  //on-click profile on view ajax
+
+  $('.selectContainer').on('click', '.imgQueqe', function(evt) {
+    evt.stopPropagation();
+    let id = {id: $(this).attr('data-id')}
+    $.ajax({
+      type: 'POST',
+      url: '/load-on-view',
+      data: id,
+      success: Render.onView
+    })
+    $(this).remove()
+  })
+
+  //on-click like ajax post
+
+  $('#like').on("click", function(evt) {
+    evt.stopPropagation();
+    if (turn) {
+      $('#green').fadeIn(200).fadeOut(200)
+      setTimeout(like, 200)
+      turn = !turn
+    }
+  });
+
+  $('#dislike').on("click", function(evt) {
+    evt.stopPropagation();
+    if (turn) {
+      $('#red').fadeIn(200).fadeOut(200)
+      setTimeout(dislike, 200)
+      turn = !turn
+    }
   });
 
   ! function () {
@@ -165,7 +212,7 @@ $(document).ready(function () {
         type: 'POST',
         url: '/get-aws-user',
         data: temp,
-        success: i === 0 ? renderOnView : renderQueqe
+        success: i === 0 ? Render.onView : Render.queqe
       });
     }
   }
@@ -178,7 +225,7 @@ $(document).ready(function () {
         type: 'POST',
         url: '/get-one',
         data: obj,
-        success: renderOne,
+        success: Render.one,
         statusCode: {
           500: function() {
             alert('severError')
